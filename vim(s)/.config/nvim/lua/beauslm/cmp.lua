d696f8dddbf3edd3815eda3296fb48dbdf1c6325
Beau McCartney <mccartney.beausl@gmail.com>
2021-10-15 20:09:03 -0600

    Add cmp path source

--- vim(s)/.config/nvim/lua/beauslm/cmp.lua
+++ vim(s)/.config/nvim/lua/beauslm/cmp.lua
@@ -3,26 +3,37 @@ local cmp = require'cmp'
 
 cmp.setup({
     enabled = true,
-snippet = {
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-  end,
-},
-sources = {
-  { name = 'nvim_lsp' },
-  { name = 'treesitter' },
-  { name = 'luasnip' },
-  { name = 'buffer' },
-  { name = 'nvim-lua' },
-  -- { name = 'crates' },
-},
-formatting = {
-  format = require("lspkind").cmp_format({with_text = true, menu = ({
-      buffer = "[Buffer]",
-      nvim_lsp = "[LSP]",
-      luasnip = "[LuaSnip]",
-      nvim_lua = "[Lua]",
-      latex_symbols = "[Latex]",
-    })}),
-},
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
+    mapping = {
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = {
+      { name = 'luasnip' },
+      { name = 'nvim_lua' },
+      { name = 'nvim_lsp' },
+      { name = 'treesitter' },
+      { name = 'path' },
+      { name = 'buffer' },
+      { name = 'vim-dadbod-completion' },
+      -- { name = 'crates' },
+    },
+    formatting = {
+      format = require("lspkind").cmp_format({with_text = true, menu = ({
+          buffer = "[Buffer]",
+          nvim_lsp = "[LSP]",
+          luasnip = "[LuaSnip]",
+          nvim_lua = "[Lua]",
+          treesitter = "[Treesitter]",
+          latex_symbols = "[Latex]",
+          path = "[Path]",
+        })}),
+    },
 })
