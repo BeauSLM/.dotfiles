6e79c00ba4beaaad924027a97cf75feb0e8ea4a8
Beau McCartney <mccartney.beausl@gmail.com>
2021-10-15 21:24:10 -0600

    Add snippet jumping

--- vim(s)/.config/nvim/lua/beauslm/cmp.lua
+++ vim(s)/.config/nvim/lua/beauslm/cmp.lua
@@ -1,4 +1,10 @@
 -- Setup nvim-cmp.
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 local cmp = require'cmp'
 
 cmp.setup({
@@ -9,6 +15,27 @@ cmp.setup({
       end,
     },
     mapping = {
+          ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
@@ -16,13 +43,13 @@ cmp.setup({
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
-      { name = 'luasnip' },
       { name = 'nvim_lua' },
+      { name = 'luasnip' },
+      { name = 'vim-dadbod-completion' },
       { name = 'nvim_lsp' },
       { name = 'treesitter' },
       { name = 'path' },
       { name = 'buffer' },
-      { name = 'vim-dadbod-completion' },
       -- { name = 'crates' },
     },
     formatting = {
@@ -30,7 +57,7 @@ cmp.setup({
           buffer = "[Buffer]",
           nvim_lsp = "[LSP]",
           luasnip = "[LuaSnip]",
-          nvim_lua = "[Lua]",
+          nvim_lua = "[Nvim-Lua]",
           treesitter = "[Treesitter]",
           latex_symbols = "[Latex]",
           path = "[Path]",
