b6941559d0773f01701b7f3472504bb0431199cc
Beau McCartney <mccartney.beausl@gmail.com>
2022-06-22 15:34:19 -0600

    polybar fixes:
    - put tray on primary monitor
    - touch up polybar config
    - add config file to dm-confedit

--- window_managers/.config/startup.sh
+++ window_managers/.config/startup.sh
@@ -8,13 +8,22 @@ run() {
   fi
 }
 
-
+# instant commands - not daemons
 polybar-msg cmd quit # close all polybars
 nvidia-settings --load-config-only # load brightness settings
 "$HOME"/.fehbg # reload wallpaper
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar &
+for m in $(polybar --list-monitors | sed 's/ //g'); do
+  n=$(echo "$m" | cut -d ":" -f1)
+  if echo "$m" | grep -q "primary"
+  then
+    bar="tray"
+    log="/polybar_tray.log"
+  else
+    bar="normal"
+    log="/polybar_normal.log"
+  fi
+  MONITOR=$n polybar -r "$bar" 2>&1 | tee -a /tmp/"$log" &
 done
 
 run dunst
