a1d21e005ff622d1bb099596704401f95b5d2c01
Beau McCartney <mccartney.beausl@gmail.com>
2022-06-24 10:14:35 -0600

    cleanup startup script

--- window_managers/.config/startup.sh
+++ window_managers/.config/startup.sh
@@ -9,26 +9,29 @@ run() {
 }
 
 # instant commands - not daemons
-polybar-msg cmd quit # close all polybars
-nvidia-settings --load-config-only # load brightness settings
-"$HOME"/.fehbg # reload wallpaper
+polybar-msg cmd quit                   # close all polybars
+nvidia-settings --load-config-only     # load brightness settings
+"$HOME"/.fehbg                         # reload wallpaper
 
-for m in $(polybar --list-monitors | sed 's/ //g'); do
+# IFS='<line feed>'
+IFS='
+'
+for m in $(polybar --list-monitors); do
   n=$(echo "$m" | cut -d ":" -f1)
   if echo "$m" | grep -q "primary"
   then
     bar="tray"
-    log="/polybar_tray.log"
+    logfile="/polybar_tray.log"
   else
     bar="normal"
-    log="/polybar_normal.log"
+    logfile="/polybar_normal.log"
   fi
-  MONITOR=$n polybar -r "$bar" 2>&1 | tee -a /tmp/"$log" &
+  MONITOR=$n polybar -r "$bar" 2>&1 | tee -a /tmp/"$logfile" &
 done
 
-run dunst
-run picom -b
-run sxhkd
-run imwheel
-run nm-applet
-run copyq
+run dunst          # notifications
+run picom -b       # compositor
+run sxhkd          # hotkey daemon
+run imwheel        # scrollwheel improvements
+run nm-applet      # network manager tray icon
+run copyq          # cool clipboard thing that I'd honestly like to get rid of
