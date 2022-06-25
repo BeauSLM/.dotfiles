d1a6ffa0813b419c62e529bf8543241d29f454d3
Beau McCartney <mccartney.beausl@gmail.com>
2022-06-25 10:21:21 -0600

    move all stowed stuff into one folder

--- stow_folder/.config/fish/sets.fish
+++ stow_folder/.config/fish/sets.fish
@@ -2,30 +2,21 @@
 
 set -x TERMINAL alacritty
 
-# add stow folders
-set -x STOW_FOLDERS "cli_utilities,dmscripts,git,other_config,shell,terminal,tmux,text_editors,window_managers"
-
-# add dotfiles
 set -x DOTFILES $HOME/.dotfiles
 
-# add source installs
 set -x SOURCE_INSTALLS $HOME/si
 
-# nvim as editor
 set -x EDITOR "hx"
 set -x VISUAL "hx"
 
-# brave as browser
 set -x BROWSER "brave"
 
-# bat as manpager
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 # set -x MANPAGER "nvim +Man!"
 
 # jdtls
 set -x JDTLS_HOME $SOURCE_INSTALLS/eclipse.jdt.ls/org.eclipse.jdt.ls.product/target/repository
 
-# helix runtime
 set -x HELIX_RUNTIME $SOURCE_INSTALLS/helix/runtime
 
 set fish_cursor_default     block      blink
