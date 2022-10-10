649fcc5dc64c55935ea0b57f597d72002269f5a5
Beau McCartney <beau@beaumccartney.xyz>
2022-10-10 12:31:19 -0600

    move to xmonad

--- stow_folder/.config/xmonad/xmonad.hs
+++ stow_folder/.config/xmonad/xmonad.hs
@@ -1,24 +1,38 @@
 -- TODO:
--- reverse layout: main monitor by default and keybinds
+-- reverse layout keybinding
 -- make window floating or floating layout
--- make bar work
 
 import XMonad
-
-import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
 import XMonad.Layout.Reflect
-
+import XMonad.Util.EZConfig
 import XMonad.Hooks.EwmhDesktops
 
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.ManageHelpers
+
+layouts = tiled ||| Full
+    where
+        tiled = reflectHoriz $ Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 3/5
+        delta = 3/100
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ className =? "Gimp" --> doFloat
+    , className =? "chaos_insurgency" --> doFloat
+    , isDialog            --> doFloat
+    ]
 
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ def
+main = xmonad 
+    . ewmhFullscreen 
+    . ewmh 
+    . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure def)) defToggleStrutsKey -- TODO: fix bar key lul
+    $ def
     { modMask = mod4Mask
     , terminal = "alacritty"
+    , layoutHook = layouts
+    , manageHook = myManageHook
     }
-
 
