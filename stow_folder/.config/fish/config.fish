5b29172c6b2f55c8bf9d05e827757fea68ebf488
Beau McCartney <beau@beaumccartney.xyz>
2023-01-22 10:27:59 -0700

    remove more stupid comments

--- stow_folder/.config/fish/config.fish
+++ stow_folder/.config/fish/config.fish
@@ -3,16 +3,12 @@ if not status is-interactive
     exit
 end
 
-# kill this jesus
 set fish_greeting
 
-# sources
 source $__fish_config_dir/colors.fish
 source $__fish_config_dir/bindings.fish
 source $__fish_config_dir/aliases.fish
 
-# zoxide
 zoxide init --cmd cd fish | source
 
-# starship
 starship init fish | source
